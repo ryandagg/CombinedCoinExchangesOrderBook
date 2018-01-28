2839d317c1cf4c4158e723c365a7bce7a5384145
Ryan Dagg <ryan.dagg@gmail.com>
2018-01-28 13:57:35 -0700

    is heroku setting BASE_URL env?

--- server/config.js
+++ server/config.js
@@ -1,11 +1,15 @@
 const port = process.env.PORT || 8000;
 const baseUrl = process.env.BASE_URL;
 const socketPort = process.env.SOCKET_PORT || 3000;
+const localHost = 'http://localhost';
+
+console.log('process.env.NODE_ENV: ', process.env.NODE_ENV)
+console.log('process.env.baseUrl: ', process.env.baseUrl)
 
 export default {
     port,
     socketPort: process.env.SOCKET_PORT || 3000,
-    baseUrl: baseUrl || `http://localhost:${port}`,
-    socketUrl: baseUrl ? `${baseUrl}:${socketPort}` : `http://localhost:${socketPort}`,
+    baseUrl: baseUrl || `${localHost}:${port}`,
+    socketUrl: `${baseUrl || localHost}:${socketPort}`,
 };
 
